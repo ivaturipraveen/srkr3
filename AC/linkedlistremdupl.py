1a96ae4b1100da3778831f4957c1beb3d223208a
ivaturipraveen <113433003+ivaturipraveen@users.noreply.github.com>
2023-11-13 14:31:55 +0530

    Delete AC directory

--- AC/linkedlistremdupl.py
+++
@@ -1,9 +0,0 @@
-linked_list = [1, 2, 2, 3, 4, 4, 5]
-unique_values = []
-
-for item in linked_list:
-    if item not in unique_values:
-        unique_values.append(item)
-
-print("Linked List after Removing Duplicates:")
-print(unique_values)
